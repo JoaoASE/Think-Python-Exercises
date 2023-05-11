8f5bee2f6de75d2912691a8338676418f46586ad
JoaoASE <jasiqueira174@gmail.com>
2023-05-11 12:20:19 -0300

    Setimo commit Lista

---
+++ Listas/ex7.py
@@ -0,0 +1,14 @@
+#exercício de tupla
+pratos_buffet=('macarrao','carne moida', 'escondidinho','salada','feijoada')
+print("-")
+for pratos in pratos_buffet:
+    print(pratos)
+
+#teste de mudança na tupla (ERR0)
+#pratos_buffet[0]='arroz'
+
+
+pratos_buffet=('arroz','frango', 'escondidinho','salada','feijoada')
+print("-")
+for pratos_revi in pratos_buffet:
+    print(pratos_revi)
