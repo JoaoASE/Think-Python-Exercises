620808b98c70ad0a759fb04a15e3869f59b72cc0
JoaoASE <jasiqueira174@gmail.com>
2023-05-11 12:18:20 -0300

    Terceiro commit Lista

---
+++ Listas/ex3.py
@@ -0,0 +1,12 @@
+squares = []   #lista sem valor
+for value in range(1,11): 
+ square = value**2
+ squares.append(square)
+ print(squares)
+
+
+
+
+
+
+#exercicio de laço e função range() com números
