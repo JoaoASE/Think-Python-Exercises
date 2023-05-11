6ee352fd2dfb08141ba749d1488460bb61ebba1f
JoaoASE <jasiqueira174@gmail.com>
2023-05-11 12:15:47 -0300

    Primeiro commit

---
+++ If/ex1.py
@@ -0,0 +1,17 @@
+#exercício de if atribuído a um laço
+
+'''cars = ['audi','bmw','honda','porsche']
+
+for car in cars:
+    if car =='bmw':
+        print(car.upper())
+    else:
+        print(car.title())'''
+
+
+pizza = 'azeitona'
+
+if (pizza =='pepperoni'):
+  print("Me dê papai")
+else:
+  print("to fora")
