9ddbe57dfa4bf5335e51247a16b67f9c22ad2ee2
JoaoASE <jasiqueira174@gmail.com>
2023-05-11 12:18:00 -0300

    Segundo commit Lista

---
+++ Listas/ex2.py
@@ -0,0 +1,9 @@
+pizzas = ['napolitana','marguerita','calabresa']
+for tipos in pizzas:
+    print(tipos)
+
+for tipos in pizzas:
+    print(tipos.title()+" Mas, gosto de pizza de pepperoni")
+
+
+#exercicio de laço for com strings
