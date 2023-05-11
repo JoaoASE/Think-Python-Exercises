a441e60a02dc43319c8c3e20d020305bacf2d1b6
JoaoASE <jasiqueira174@gmail.com>
2023-05-11 12:17:31 -0300

    Primeiro commit Lista

---
+++ Listas/ex1.py
@@ -0,0 +1,32 @@
+#var = "Hello world"
+#splited =  var.split()
+#print(splited)
+
+"""lista = ['joão','Ester','Edgar','Leolina']
+print("Olá " + lista[0].upper())
+lista[0]="Pedro"
+print(lista[0])
+print("Olá " + lista[0].upper())
+lista.append('João')
+print(lista)
+del lista[0]
+print(lista)
+popped_list = lista.pop()
+print(popped_list)"""
+
+'''lista_jantar = ['Amor','Sogra','Pai','Mãe','Ester']
+print("Olá, você está convidado para o jantar,"+lista_jantar[0])
+del lista_jantar[1]
+print(lista_jantar)
+lista_jantar.append('Kelyson')
+print(lista_jantar)
+print(len(lista_jantar))'''
+
+viagem_sonho=['San Francisco','London','Paris']
+print(viagem_sonho)
+print(sorted(viagem_sonho))
+sorted_viagem_sonho = sorted(viagem_sonho, reverse=True)
+print(sorted_viagem_sonho)
+print(viagem_sonho)
+viagem_sonho.reverse()
+print(viagem_sonho)
