38719647cee94af5729cdf0fda24d6a3d4bc0864
JoaoASE <jasiqueira174@gmail.com>
2023-05-12 16:53:32 -0300

    Commit

---
+++ Dic/ex2.py
@@ -0,0 +1,7 @@
+pessoa_1 = {'nome':'Pedro','cor_favorita':'preto'}
+pessoa_2 = {'Nome':'Julia','cor_favorita':'branco'}
+pessoa_3 = {'Nome':'Maria','cor_favorita':'amarelo'}
+pessoa_4 = {'Nome':'Isabely','cor_favorita':'preto'}
+pessoa_5 = {'Nome':'Joao','cor_favorita':'preto'}
+
+print(pessoa_2['cor_favorita'])
