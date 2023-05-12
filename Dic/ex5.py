33baeef7aa4e93f17ae9377a393970d8d2ec7161
JoaoASE <jasiqueira174@gmail.com>
2023-05-12 16:54:54 -0300

    Commit

---
+++ Dic/ex5.py
@@ -0,0 +1,10 @@
+#percorrendo um dicionario inteiro dentro de uma lista
+
+alien_0 = {'name':'gamora','color':'green'}
+alien_1 = {'name':'groot','color':'brown'}
+alien_2 = {'name':'mantis','color':'pink'}
+
+aliens = [alien_0,alien_1,alien_2]
+
+for alien in aliens:
+    print(alien)
