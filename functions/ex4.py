7369f81dd4bfbfd80394043e0a41392dd4053fba
JoaoASE <jasiqueira174@gmail.com>
2023-05-18 14:10:40 -0300

    functions 4

---
+++ functions/ex4.py
@@ -0,0 +1,6 @@
+#Escreva uma função chamada make_shirt() que aceite um tamanho e o texto de uma mensagem que deverá ser estampada na camiseta.
+#A função deve exibir uma frase que mostre o tamanho da camiseta e a mensagem estampada.
+
+def make_shirt(size_shirt,description):
+    print("This shirt is " + size_shirt + description)
+make_shirt('13', ' diameter')
