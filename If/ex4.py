f62ccac209e45638b47a29ac4e7f395f36b03228
JoaoASE <jasiqueira174@gmail.com>
2023-05-18 14:06:00 -0300

    Ex 4

---
+++ If/ex4.py
@@ -0,0 +1,12 @@
+#exercicio com if e entrada input() com módulo e resto
+
+number = int(input('Enter a number and will see if it is even: '))
+
+if number % 2 == 0:
+    print('The number '+str(number) + ' is even.')
+else:
+    print('The number '+ str(number) + ' is odd.')
+
+
+
+#calcula se o numero que entrou é divisivel por 2. Se for é par e se for é ímpar
